27dbe465ac6c6c2faa341c6766a63de0546347c9
maria <m.henao5@utp.edu.co>
2024-07-28 23:28:57 -0500

    ejercicios realizados: 2,4,6,8

---
+++ ejercicio8.js
@@ -0,0 +1,20 @@
+/*Sumar un rango de numeros
+
+Escribir una función llamada sumarRango que reciba dos argumentos: número inicial y número final.
+ La función debe retornar la suma de los números en ese rango (incluyéndolos).
+
+Nota: puedes asumir que el número inicial va a ser menor o igual que el número final.
+*/
+
+function sumarRango(numInicial, numFinal){
+    let resultado = 0;
+    for(let i = numInicial; i <= numFinal; i++){
+        resultado += i;
+    }
+    return resultado;
+}
+
+// código de prueba
+console.log(sumarRango(0, 10)) // 55
+console.log(sumarRango(12, 14)) // 39
+console.log(sumarRango(5, 5)) // 5
