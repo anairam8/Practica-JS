dd30c96534f91064d7a490e5bffcb43b49ad967c
JuanValencia19 <juan.valencia10@utp.edu.co>
2024-08-19 20:18:04 -0500

    Realice los ejercicios 7,15,17 del taller2

---
+++ Taller_2/#15/ejercicio15.js
@@ -0,0 +1,9 @@
+
+    let numeroRandom = Math.random()*20;
+    numeroRandom = Math.floor(numeroRandom);
+    document.write(numeroRandom);
+    if(numeroRandom %  2 == 0){
+        console.log(true);
+    } else {
+        console.log(false);
+    }
